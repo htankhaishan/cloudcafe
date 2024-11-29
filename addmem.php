7a76c1e07fd562dcab52af4104006f22ac35842c
Kyaw Htet Thu <htankhaishan@gmail.com>
2024-11-29 12:05:06 +0800

    Update addmem.php

--- addmem.php
+++ addmem.php
@@ -1,20 +1,38 @@
-<?php
+<?php 
 session_start();
 
 include('connection.php');
-	
-$studentnum=$_POST['studentnum'];
-$name=$_POST['name'];
-$surname=$_POST['surname'];
-$contacts=$_POST['contacts'];
-$password=$_POST['password'];
-$email=$_POST['email'];
 
-$sql= "INSERT INTO members (studentnum, name, surname, contacts, password,  email) VALUES ('$studentnum', '$name', '$surname', '$contacts', '$password', '$email')";
-if(mysql_query($sql)){
-header("location: loginindex.php");
+// Retrieve form data
+$studentnum = $_POST['studentnum'];
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$contacts = $_POST['contacts'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+
+// Prepare and execute the SQL query using mysqli
+$sql = "INSERT INTO members (studentnum, name, surname, contacts, password, email) VALUES (?, ?, ?, ?, ?, ?)";
+$stmt = $bd->prepare($sql);
+
+if ($stmt) {
+    // Bind parameters
+    $stmt->bind_param("ssssss", $studentnum, $name, $surname, $contacts, $password, $email);
+    
+    // Execute the statement
+    if ($stmt->execute()) {
+        // Redirect on success
+        header("Location: loginindex.php");
+        exit;
+    } else {
+        // Log and display error message
+        echo "Error: " . $stmt->error;
+    }
+} else {
+    // Log and display error in statement preparation
+    echo "Error in SQL preparation: " . $bd->error;
 }
-echo die("could not connect". mysql_error());
 
-mysql_close($con);
-?> 
+// Close the database connection
+$bd->close();
+?>
