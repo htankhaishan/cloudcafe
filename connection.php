38d069036321418ce1ce5ff661ccd98eaa68a918
Kyaw Htet Thu <htankhaishan@gmail.com>
2024-11-29 12:06:37 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,21 @@
 <?php
-$mysql_hostname = "localhost";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);  // Show errors for debugging
+
+$mysql_hostname = "127.0.0.1";
 $mysql_user = "root";
 $mysql_password = "";
 $mysql_database = "wings";
-$prefix = "";
-$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
-mysql_select_db($mysql_database, $bd) or die("Could not select database");
+
+// Create a new MySQLi connection
+$bd = new mysqli($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
+
+// Check for a connection error
+if ($bd->connect_error) {
+    die("Connection failed: " . $bd->connect_error);
+}
+
+// If the connection is successful, select the database (mysqli automatically does this on connection)
+echo "Connected successfully to the database '$mysql_database'";
 
 ?>
