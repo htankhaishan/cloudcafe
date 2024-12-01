b2e40549e14066e82775e23fe45ddf45360588a3
Kyaw Htet Thu <htankhaishan@gmail.com>
2024-12-01 16:30:26 +0800

    auth session logout

--- auth.php
+++ auth.php
@@ -1,18 +1,22 @@
 <?php
 session_start();
 
-// Check if the user clicked on the logout link
-if (isset($_GET['logout'])) {
-    // Destroy the session and logout the user
-    session_unset();  // Unset all session variables
-    session_destroy();  // Destroy the session
-    header("Location: admin_index.php"); // Redirect to the login page
+// Logout the user if 'logout' is set in the URL query string
+if (isset($_GET['logout']) && $_GET['logout'] == 'true') {
+    // Unset all session variables
+    session_unset();
+    
+    // Destroy the session
+    session_destroy();
+    
+    // Redirect to the login page (admin_index.php)
+    header("Location: admin_index.php");
     exit();
 }
 
-// Redirect to login page if the user is not logged in
+// Redirect to the login page if the user is not logged in
 if (!isset($_SESSION['SESS_MEMBER_ID']) || trim($_SESSION['SESS_MEMBER_ID']) === '') {
-    header("Location: admin_index.php"); // Redirect to the login page
+    header("Location: admin_index.php"); // Redirect to login page
     exit();
 }
 ?>
