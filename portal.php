c620c81eaaae91bc80c6166b26250f365cd5939d
Kyaw Htet Thu <htankhaishan@gmail.com>
2024-12-01 12:01:37 +0800

    portal.php

--- portal.php
+++ portal.php
@@ -4,87 +4,89 @@ require_once('auth.php');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Wings Cafe</title>
-<style type="text/css">
-<!--
-.style1 {
-	color: #000000;
-	font-weight: bold;
-	font-size: 24px;
-}
--->
-</style>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <title>Wings Cafe</title>
+    <style type="text/css">
+        <!--
+        .style1 {
+            color: #000000;
+            font-weight: bold;
+            font-size: 24px;
+        }
+        -->
+    </style>
 </head>
 
 <body>
-<form action="saveorder.php" method="post">
-<input name="id" type="hidden" value="<?php echo $_SESSION['SESS_MEMBER_ID']; ?>" />
-<input name="transcode" type="hidden" value="<?php echo $_SESSION['SESS_FIRST_NAME']; ?>" />
-<table width="400" border="0" cellpadding="0" cellspacing="0">
-<?php
+    <form action="saveorder.php" method="post">
+        <input name="id" type="hidden" value="<?php echo htmlspecialchars($_SESSION['SESS_MEMBER_ID']); ?>" />
+        <input name="transcode" type="hidden" value="<?php echo htmlspecialchars($_SESSION['SESS_FIRST_NAME']); ?>" />
+        
+        <table width="400" border="0" cellpadding="0" cellspacing="0">
+            <?php
+            if (isset($_GET['id'])) {
+                include('connection.php');
+                $id = $_GET['id'];
 
-				  if (isset($_GET['id']))
-			{
-	
-	
-			include('connection.php');
-		
-			$id=$_GET['id'];
-			$result = mysql_query("SELECT * FROM products WHERE product_id = $id");
+                // Securely prepare and execute the SQL query using mysqli
+                $stmt = $conn->prepare("SELECT * FROM products WHERE product_id = ?");
+                $stmt->bind_param("i", $id);
+                $stmt->execute();
+                $result = $stmt->get_result();
 
-			while($row3 = mysql_fetch_array($result)){
-			$id = $row3['id'];
+                // Fetch and display product details
+                while ($row3 = $result->fetch_assoc()) {
+                    $id = $row3['id'];
 
-    echo '<tr>';
-    echo '<td width="80"><img src="images/bgr/'.$row3['product_photo'].'" /></td>';
-    echo '<td width="200"><span class="style1">'.'</span></td>';
-	echo '<td width="120"></span></td>';
-  echo '</tr>';
-  echo '<tr>';
-    echo '<td width="80"><input name="name" type="text" value="'.$row3['name'].'" readonly/><input name="id" type="hidden" value="'.$id.'"/></td>';
-	echo '<td width="120"></span></td>';
-  echo '</tr>';
-  }
-		}	?>
+                    echo '<tr>';
+                    echo '<td width="80"><img src="images/bgr/' . htmlspecialchars($row3['product_photo']) . '" alt="Product Image" /></td>';
+                    echo '<td width="200"><span class="style1">' . htmlspecialchars($row3['name']) . '</span></td>';
+                    echo '<td width="120"></td>';
+                    echo '</tr>';
+                    echo '<tr>';
+                    echo '<td width="80">
+                            <input name="name" type="text" value="' . htmlspecialchars($row3['name']) . '" readonly/>
+                            <input name="id" type="hidden" value="' . htmlspecialchars($id) . '"/>
+                          </td>';
+                    echo '<td width="120"></td>';
+                    echo '</tr>';
+                }
+                $stmt->close();
+            }
+            ?>
+        </table>
+        <br />
+        <label style="color:#000000;"></label>
+        <br />
+        <table width="400" border="0" cellpadding="0" cellspacing="0" style="color:#000000;"> 
+            <tr>
+                <td width="128">Price</td>
+                <td width="93">Quantity</td>
+            </tr>
+            <?php
+            if (isset($_GET['id'])) {
+                include('connection.php');
+                $id = $_GET['id'];
 
-</table>
-<br />
-  <label style="color:#000000;"></label>
-<br />
-<table width="400" border="0" cellpadding="0" cellspacing="0" style="color:#000000;"> 
-  <tr>
-   
-    <td width="128">Price</td>
-    <td width="93">Quantity</td>
-  </tr>
-  <?php
+                // Securely fetch product details again for pricing and quantity form
+                $stmt = $conn->prepare("SELECT * FROM products WHERE product_id = ?");
+                $stmt->bind_param("i", $id);
+                $stmt->execute();
+                $result = $stmt->get_result();
 
-				  if (isset($_GET['id']))
-			{
-	
-	
-			include('connection.php');
-
-		
-			$id=$_GET['id'];
-			$result = mysql_query("SELECT * FROM products WHERE product_id = $id");
-
-			while($row3 = mysql_fetch_array($result))
-  			{
-			
-  echo '<tr>';
-    echo '<td>'.$row3['price'].'</td>';
-	echo "<input type='hidden' name='price' value='".$row3['price']."'>";
-	echo "<input type='hidden' name='name' value='".$row3['name']."'>";
-	echo "<td><input type='text' size='5' name='quantity'></td>";
-    echo '<td>'.'<input name="butadd" type="image" value="'.$row3['id'].'" src="images/button.png" />'.'</td>';
-  echo '</tr></form>';
-  }
-  
-  			}
-			?>
-</table>
-</form>
+                while ($row3 = $result->fetch_assoc()) {
+                    echo '<tr>';
+                    echo '<td>' . htmlspecialchars($row3['price']) . '</td>';
+                    echo '<input type="hidden" name="price" value="' . htmlspecialchars($row3['price']) . '">';
+                    echo '<input type="hidden" name="name" value="' . htmlspecialchars($row3['name']) . '">';
+                    echo '<td><input type="text" size="5" name="quantity"></td>';
+                    echo '<td><input name="butadd" type="image" value="' . htmlspecialchars($row3['id']) . '" src="images/button.png" /></td>';
+                    echo '</tr>';
+                }
+                $stmt->close();
+            }
+            ?>
+        </table>
+    </form>
 </body>
 </html>
