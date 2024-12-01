76dc379ff27cc854c22884953dd93eb22d807aa3
Kyaw Htet Thu <htankhaishan@gmail.com>
2024-12-01 14:01:42 +0800

    a

--- admin.php
+++ admin.php
@@ -1,18 +1,32 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);  // Show errors for debugging
+session_start();
 
-$mysql_hostname = "127.0.0.1";
-$mysql_user = "root";
-$mysql_password = "@Bcde12345";
-$mysql_database = "wings";
+include("connection.php");
+$id = $_POST['email'];
+$password = $_POST['password'];
+$id = mysql_real_escape_string($id);
+$password = mysql_real_escape_string($password);
 
-try {
-    // Create a new PDO connection and assign it to $db
-    $db = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_database;charset=utf8", $mysql_user, $mysql_password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Enable exceptions for errors
-    echo "Connected successfully to the database '$mysql_database'";
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+$sql = "select * from users where email = '$id' and password = '$password'";
+$result = mysql_query($sql);
+$count = mysql_num_rows($result);
+if($count ==1){
+// session_regenerate_id();
+			session_regenerate_id();
+			$member = mysql_fetch_assoc($result);
+			$_SESSION['SESS_MEMBER_ID'] = $member['id'];
+			$_SESSION['SESS_FIRST_NAME'] = $member['username'];
+			session_write_close();
+header("location:home_admin.php");
+
+}
+else{
+echo "<h4 style='color:red;'>";
+			
+			
+			echo "Please enter your correct login details!!!";
+			echo "</h4>";
+die(mysql_error());
+			
 }
 ?>
