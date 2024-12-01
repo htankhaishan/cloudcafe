66435dd2773808eed8523d0e0094d06d2e6030b6
Kyaw Htet Thu <htankhaishan@gmail.com>
2024-12-01 12:26:25 +0800

    loginexec.php

--- loginexec.php
+++ loginexec.php
@@ -38,7 +38,7 @@ $password = clean($_POST['password']);
 try {
     // Use a prepared statement to prevent SQL injection
     $qry = "SELECT * FROM members WHERE email = :email AND password = :password";
-    $stmt = $bd->prepare($qry);
+    $stmt = $pdo->prepare($qry); // Use $pdo instead of $bd
     $stmt->bindParam(':email', $login);
     $stmt->bindParam(':password', $password);
 
